6be2310f1dc1138dec96fed83fb0d7768f14a51a
younghyun-na <younghyunna@naver.com>
2021-11-18 06:52:54 +0900

    feat: 회원가입

--- src/main/java/com/mintiz/domain/repository/UserRepository.java
+++ src/main/java/com/mintiz/domain/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.mintiz.domain.repository;
 
 import com.mintiz.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
 }
