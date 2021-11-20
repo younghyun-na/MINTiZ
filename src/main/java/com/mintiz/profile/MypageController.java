9d0cbc394f920b3a792a829905aff03e4e912910
jimin3263 <dkdldsla0000@naver.com>
2021-11-20 19:36:56 +0900

    feat: 유저 프로필 조회

--- src/main/java/com/mintiz/profile/MypageController.java
+++ src/main/java/com/mintiz/profile/MypageController.java
@@ -3,10 +3,12 @@ package com.mintiz.profile;
 import com.mintiz.domain.Post;
 import com.mintiz.domain.User;
 import com.mintiz.user.UserService;
+import com.sun.xml.bind.v2.TODO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,24 +25,7 @@ public class MypageController {
     @GetMapping("/mypage")
     public String mypage(Model model){
 
-        User user = userService.findUser(id);
-        model.addAttribute("user",user);
-        List<Post> posts = user.getPosts();
-
-        List<Integer> postMonth = new ArrayList<>();
-        List<Integer> postDate = new ArrayList<>();
-        List<Integer> postYear = new ArrayList<>();
-        LocalDateTime createdDate;
-        for (Post post : posts) {
-            createdDate = post.getCreatedAt();
-            postMonth.add(createdDate.getMonth().getValue());
-            postDate.add(createdDate.getDayOfMonth());
-            postYear.add(createdDate.getYear());
-        }
-
-        model.addAttribute("postMonth",postMonth);
-        model.addAttribute("postDate",postDate);
-        model.addAttribute("postYear",postYear);
+        getMint(model);
 
         return "MyPage";
     }
@@ -54,5 +39,35 @@ public class MypageController {
     public String bookmark(Model model){
         return "BookMark";
     }
+
+    @GetMapping("/profile/{userId}")
+    public String profile(@PathVariable Long userId, Model model){
+        getMint(model);
+        return "Profile";
+    }
+
+    //TODO userId 넘기기
+
+    private void getMint(Model model) {
+        User user = userService.findUser(id);
+        model.addAttribute("user", user);
+        List<Post> posts = user.getPosts();
+
+        List<Integer> postMonth = new ArrayList<>();
+        List<Integer> postDate = new ArrayList<>();
+        List<Integer> postYear = new ArrayList<>();
+        LocalDateTime createdDate;
+        for (Post post : posts) {
+            createdDate = post.getCreatedAt();
+            postMonth.add(createdDate.getMonth().getValue());
+            postDate.add(createdDate.getDayOfMonth());
+            postYear.add(createdDate.getYear());
+        }
+
+        model.addAttribute("postMonth", postMonth);
+        model.addAttribute("postDate", postDate);
+        model.addAttribute("postYear", postYear);
+    }
+
 
 }
