0d67815525d0451232a437c12e6dff240753c45e
semi_cloud <sksmsrkdtpal@naver.com>
2021-11-25 01:33:23 +0900

    fix: writingdetails.html 이미지 슬라이드 수정

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -12,3 +12,36 @@ function addPostScriptTag() {
     }
 
 }
+
+const input = document.querySelector('input');
+const preview = document.querySelector('.preview');
+
+input.addEventListener('change', showImageFile);
+
+function showImageFile() {
+    const selectedFiles = input.files;
+    const list = document.createElement('ul');
+    preview.appendChild(list);
+
+    for(const file of selectedFiles) {
+        const listItem = document.createElement('li');
+        if(validFileType(file)) {
+            const summary = document.createElement('div');
+            summary.textContent = `파일명 : ${file.name}`;
+            listItem.appendChild(summary);
+        } else {
+            const message = document.createElement('div');
+            message.textContent = `파일명 ${file.name}: .jpg/png 파일을 선택하세요`;
+            listItem.appendChild(message);
+        }
+        list.appendChild(listItem);
+    }
+}
+const fileTypes = [
+    'image/png',
+    'image/jpeg'
+];
+
+function validFileType(file) {
+    return fileTypes.includes(file.type);
+}
